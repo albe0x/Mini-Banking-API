5d9c6c253a950f1dd09d733b5faa63a64aa22d5e
albe0x <student@itismeucci.com>
2026-04-01 12:42:14 +0200

    created RESTFUL API endpoints

--- php/index.php
+++ php/index.php
@@ -5,11 +5,12 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/controllers/AlunniController.php';
+require __DIR__ . '/controllers/TransactionsController.php';
+require __DIR__ . '/controllers/ConversionController.php';
 
 $app = AppFactory::create();
 
-$app->get('/test', function (Request $request, Response $response, array $args) {
+$app->get('/accounts/1/balance/convert/crypto?to=BTC', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("Test page");
     return $response;
 });
@@ -20,6 +21,16 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response;
 });
 
-$app->get('/alunni', "AlunniController:index");
+$app->get('/accounts/1/transactions',       "ConversionController:getTransaction");
+$app->get('/accounts/1/transactions/5',     "ConversionController:getTransactionNumber");
+$app->post('/accounts/1/deposits ',         "ConversionController:makeDepositit");
+$app->post('/accounts/1/withdrawals',       "ConversionController:makeWithdrawal");
+$app->put('/accounts/1/transactions/5 ',    "ConversionController:editTransactionNumber");
+$app->delete('/accounts/1/transactions/5',  "ConversionController:deleteTransactionNumber");
+
+$app->get('/accounts/1/balance',            "ConversionController:getBalance");
+
+$app->get('/accounts/1/balance/convert/crypto?to=BTC', "TransactionsController:fiat");
+$app->get('/accounts/1/balance/convert/crypto?to=BTC', "TransactionsController:crypto");
 
 $app->run();
