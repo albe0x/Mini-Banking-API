5ae5b446b7d1db07055b9543e81e6fa392b3257a
albe0x <student@itismeucci.com>
2026-04-01 13:18:10 +0200

    ciao

--- php/controllers/TransactionsController.php
+++ php/controllers/TransactionsController.php
@@ -4,16 +4,18 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class TransactionsController
 {
+/*   
   public function index(Request $request, Response $response, $args){
-    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
-    $result = $mysqli_connection->query("SELECT * FROM alunni");
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'banking');
+    $result = $mysqli_connection->query("SELECT * FROM accounts");
     $results = $result->fetch_all(MYSQLI_ASSOC);
 
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+*/
 
-  public function getTransaction(Request $request, Response $response, $args){}
+    public function getTransaction(Request $request, Response $response, $args){}
 
     public function getTransactionNumber(Request $request, Response $response, $args){}
 
